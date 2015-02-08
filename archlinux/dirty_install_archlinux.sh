e234c7e0e421c9d508e40475ddcbc30de85d24f7
tdubourg <tdubourg@users.noreply.github.com>
2015-02-08 15:21:00 -0800

    Quick & Dirty install & launch script for ArchLinux. Helpful until a proper installation setup works on non-Debian.

---
+++ archlinux/dirty_install_archlinux.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+echo "Note: You need Python PIP, if not installed, this script is going to crash, run sudo pacman -Sy python2-pip and launch the script again if it happens."
+
+sudo pip2.7 install requests docopt BeautifulSoup setproctitle
+
---
+++ archlinux/dirty_launch_archlinux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+cd ../pulseaudio_dlna
+PYTHONPATH=`pwd`/../ python2 __main__.py
